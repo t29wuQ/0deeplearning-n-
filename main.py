749946d6830012c9d5e0c3d34298966627808135
K.Tsukimoto <t.29.wuq@gmail.com>
2020-10-19 21:43:47 +0900

    :cat: にゃーん(にゃーん)

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import numpy as np
+from mnist import load_mnist
+
+
+# perceptron
 
 
 def AND(x1, x2):
@@ -41,6 +45,56 @@ def XOR(x1, x2):
     return y
 
 
+# activation function
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+# neural network
+
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = a3
+    return y
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
 print('AND')
 print(AND(0, 0))
 print(AND(1, 0))
@@ -61,3 +115,16 @@ print(XOR(0, 0))
 print(XOR(1, 0))
 print(XOR(0, 1))
 print(XOR(1, 1))
+
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
+
+(x_train, t_train), (x_test, t_test) = \
+        load_mnist(flatten=True, normalize=False)
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
