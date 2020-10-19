85340bd062b044472ec07006510b8cdb7d3ff06b
K.Tsukimoto <t.29.wuq@gmail.com>
2020-10-19 22:19:11 +0900

    ばちばちばっち

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 from mnist import load_mnist
 
@@ -60,41 +61,6 @@ def relu(x):
     return np.maximum(0, x)
 
 
-# neural network
-
-
-def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network['b3'] = np.array([0.1, 0.2])
-    return network
-
-
-def forward(network, x):
-    W1, W2, W3 = network['W1'], network['W2'], network['W3']
-    b1, b2, b3 = network['b1'], network['b2'], network['b3']
-
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = a3
-    return y
-
-
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-    return y
-
-
 print('AND')
 print(AND(0, 0))
 print(AND(1, 0))
@@ -117,14 +83,50 @@ print(XOR(0, 1))
 print(XOR(1, 1))
 
 
-network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
-
 (x_train, t_train), (x_test, t_test) = \
-        load_mnist(flatten=True, normalize=False)
+        load_mnist(normalize=True, flatten=True, one_hot_label=False)
 print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
 print(t_test.shape)
+
+
+# neural network
+
+
+def init_network():
+    with open('sample_weight.pkl', 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = a3
+    return y
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
+network = init_network()
+accuracy_cnt = 0
+batch_size = 100
+for i in range(len(x_test)):
+    y = predict(network, x_test[i:i + batch_size])
+    p = np.argmax(y, axis=1)
+    accuracy_cnt += np.sum(p == t_test[i:i + batch_size])
+
+print('Accuracy:' + str(float(accuracy_cnt) / len(x_test)))
